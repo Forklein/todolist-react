00c36afba7e4dde48b919e980e15b5af0fc262c4
Forklein <giuseppepisani2@gmail.com>
2022-01-12 12:21:17 +0100

    Try to add with classic push

--- src/App.js
+++ src/App.js
@@ -4,11 +4,6 @@ import { Counter } from './features/counter/Counter';
 import { useState, useEffect } from 'react';
 
 function App() {
-  const [todolist, setTodolist] = useState([]);
-  useEffect(() => {
-    // alert('Hi Forklein');
-  }, [todolist]);
-
   const staticTodolist = [
     {
       name: 'Sveglia',
@@ -22,16 +17,39 @@ function App() {
       name: 'Programmare',
       id: 3,
     },
-  ]
+  ];
+
+  const [todolist, setTodolist] = useState([]);
+  useEffect(() => {
+    setTodolist(staticTodolist);
+  }, []);
+
+  const [input, setInput] = useState('');
+
+  const saveValue = (e) => {
+    setInput(e.target.value);
+  }
+
+  const getValue = () => {
+    todolist.push({ name: input, id: 4 });
+  }
 
 
   return (
     <div className="container text-center">
       <h1>My TodoList</h1>
+      <div className="box my-2">
+        <div className="row">
+          <div className="col-6 mx-auto">
+            <input type="text" value={input} onChange={saveValue}></input>
+            <button onClick={getValue} className="btn btn-success mx-2">Add</button>
+          </div>
+        </div>
+      </div>
       <ul className="list-group list-group-flush">
-        {staticTodolist.map((todo) => <li key={todo.id} className="list-group-item">{todo.name}</li>)}
+        {todolist.map((todo) => <li key={todo.id} className="list-group-item">{todo.name}</li>)}
       </ul>
-    </div>
+    </div >
   );
 }
 
