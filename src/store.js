3f2746503e6aa89f77be100449baf6361bb6d02f
Forklein <giuseppepisani2@gmail.com>
2022-01-20 11:35:32 +0100

    Add store js for reducer

---
+++ src/store.js
@@ -0,0 +1,34 @@
+import { createReducer } from '@reduxjs/toolkit';
+import { useReducer } from 'react';
+import { createStore } from 'redux';
+
+const staticTodolist = [
+    {
+        name: 'Sveglia',
+        id: 1,
+    },
+    {
+        name: 'Colazione',
+        id: 2,
+    },
+    {
+        name: 'Programmare',
+        id: 3,
+    },
+];
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case addTodo:
+            const newTodo = {
+                name: action.payload.name,
+                id: staticTodolist.length + 1
+            }
+            return [...state, newTodo];
+        default:
+            return state;
+    }
+}
+
+
+export const store = createStore(reducer, staticTodolist);
