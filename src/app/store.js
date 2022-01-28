2aebfa41b5edf6f33f73aa937bc88a1b18a3725d
Forklein <giuseppepisani2@gmail.com>
2022-01-28 17:53:04 +0100

    add Middleware

--- src/app/store.js
+++ src/app/store.js
@@ -2,9 +2,19 @@ import { configureStore } from '@reduxjs/toolkit';
 import todosReducer from '../features/todos/todosSlice';
 import filterReducer from '../features/todos/filterSlice';
 
+const myLog = store => next => action => {
+  console.log(action.type);
+  console.log(store.getState());
+
+  const res = next(action);
+  console.log(action.payload);
+  return res;
+}
+
 export const store = configureStore({
   reducer: {
     todos: todosReducer,
     filter: filterReducer
   },
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(myLog)
 });
